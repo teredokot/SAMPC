02d9513ab96278310b3f11f5353d4269d608b8c9
RD42 <42702181+dashr9230@users.noreply.github.com>
2020-04-11 14:26:15 +0200

    Fix bad parameter count crash when using format()

--- server/scrcustom.cpp
+++ server/scrcustom.cpp
@@ -2790,7 +2790,7 @@ static cell AMX_NATIVE_CALL n_printf(AMX *amx, cell *params)
 // native format(output[], len, const format[], {Float,_}:...)
 static cell AMX_NATIVE_CALL n_format(AMX *amx, cell *params)
 {
-	CHECK_PARAMS(amx, "format", 53);
+	CHECK_PARAMS_BETWEEN(amx, "format", 3, 53);
 	int len;
 	return set_amxstring(amx, params[1], format_amxstring(amx, params, 3, len), params[2]);
 }
